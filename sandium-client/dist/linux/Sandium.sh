75db42a02d28ecee4ca1a6e53116a0fd42bdc910
crankybunny <john_nospam@newonic.com>
2024-10-30 22:25:08 -0600

    Added GLFW 3.4 to project. Created class to resolve library path.

--- sandium-client/dist/linux/Sandium.sh
+++ sandium-client/dist/linux/Sandium.sh
@@ -2,5 +2,4 @@
 
 SANDIUM_HOME=$(dirname "$(realpath "$0")")
 cd $SANDIUM_HOME
-export LD_LIBRARY_PATH=lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-jre/bin/java -splash:lib/Splash.jpg -Xmx1G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar lib/Sandium.jar
+jre/bin/java -splash:lib/Splash.jpg --enable-native-access=ALL-UNNAMED -Xmx1G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar lib/Sandium.jar
--- sandium-client/dist/windows/Sandium.cmd
+++ sandium-client/dist/windows/Sandium.cmd
@@ -1,5 +1,4 @@
 @echo off
 pushd "%~dp0"
-set PATH=lib;%PATH%
-jre\bin\java -splash:lib/Splash.jpg -Xmx1G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar lib/Sandium.jar
+jre\bin\java -splash:lib/Splash.jpg --enable-native-access=ALL-UNNAMED -Xmx1G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar lib/Sandium.jar
 popd
